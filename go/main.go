950280439fca9327d297ced6cf4e5441ca9a5c13
Bryant Hagadorn <blhagadorn@gmail.com>
2020-01-24 18:23:22 -0500

    Modified main.go with TODO

--- go/main.go
+++ go/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/sfreiberg/gotwilio"
 	"github.com/subosito/gotenv"
 )
+
+type textRequestStruct struct {
+	From string
+	To   string
+	Body string
+}
 
 func init() {
 	err := gotenv.Load(".env")
@@ -16,12 +26,35 @@ func init() {
 }
 
 func main() {
+	http.HandleFunc("/healthz", healthz)
+	http.HandleFunc("/text", textHandler)
+	log.Fatal(http.ListenAndServe(":8087", nil))
+}
+
+func healthz(rw http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(rw, "Hello, %s!", req.URL.Path[1:])
+}
+
+func textHandler(rw http.ResponseWriter, req *http.Request) {
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(body))
+	var textRequest textRequestStruct
+	err = json.Unmarshal(body, &textRequest)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(textRequest.From, textRequest.To, textRequest.Body)
+	//TODO add better error handling
+	sendText(textRequest.From, textRequest.To, textRequest.Body)
+}
+
+func sendText(from string, to string, body string) {
 	accountSid := os.Getenv("ACCOUNT_SID")
 	authToken := os.Getenv("AUTH_TOKEN")
 	twilio := gotwilio.NewTwilioClient(accountSid, authToken)
-
-	from := os.Getenv("FROM_NUMBER")
-	to := os.Getenv("TO_NUMBER")
-	message := "Testing Twilio Microservice"
-	twilio.SendSMS(from, to, message, "", "")
+	//TODO Sanitize from, to, and body
+	twilio.SendSMS(from, to, body, "", "")
 }
