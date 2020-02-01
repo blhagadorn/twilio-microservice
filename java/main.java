73935834801819af473433a73ff276a91387a6a7
hauze-v <hauze_v@protonmail.com>
2020-02-01 15:20:00 -0500

    Added postman directory with collection and java directory for java implementation
    Signed-off-by: hauze-v <hauze_v@protonmail.com>

---
+++ java/main.java
@@ -0,0 +1,6 @@
+
+/* Exchange a hello world with twillio API, get it going, hardcode API keys and stuff
+    do springboot version - start with GET, then move to a full POST
+    then dockerize it
+*/
+
